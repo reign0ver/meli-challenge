6a817db427c400463f108246132d295cce95b338
Andrés <iamandres17@gmail.com>
2024-09-21 15:25:13 -0500

    Create initial project

---
+++ MeliChallenge/MeliChallenge/MeliChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeliChallengeApp.swift
+//  MeliChallenge
+//
+//  Created by Andrés Carrillo on 21/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MeliChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

306114c780228e37b06fbe32c9517d6725b62d9f
Andrés <iamandres17@gmail.com>
2024-09-21 17:58:56 -0500

    Create generic and decoupled HTTPClient alongside the concrete URLSessionHTTPClient implementation

---
+++ MeliChallenge/MeliChallenge/NetworkServices/HTTPClient.swift
@@ -0,0 +1,19 @@
+//
+//  HTTPClient.swift
+//  MeliChallenge
+//
+//  Created by Andrés Carrillo on 21/09/24.
+//
+
+import Foundation
+
+public protocol HTTPClientTask {
+    func cancel()
+}
+
+public protocol HTTPClient {
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+    
+    @discardableResult
+    func get(from url: URL, completion: @escaping (Result) -> Void) -> HTTPClientTask
+}
